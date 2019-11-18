35f886a696d560cf24b61b79fe8a25ec9f0ef4f1
Josh Kiger <31903268+jkiger@users.noreply.github.com>
2019-11-17 21:30:26 -0500

    Added functions for game names and executable
    The C++ files contains functions were the first one grabs all the game names from a given file path and stores them all to a list and the second function grabs and runs a game executable given a string name of a game.

---
+++ UINavigator.cpp
@@ -0,0 +1,62 @@
+#include "UINavigator.h"
+
+vector<string> UINavigator::GameList() 
+{
+
+	//create vector to store game names and returning
+	vector<string> games;
+
+	//Have to edit hardcoded path for aracade cabinet games folder 
+	std::string path = "//FRANK/U/jkiger/Documents/Games";
+
+	//Loop over all files in the given file path above
+	for (const auto & entry : directory_iterator(path)) 
+	{
+
+		//add game names to vector 
+		games.push_back(entry.path().filename().string());
+	}
+
+	//return list of game names 
+	return games;
+}
+
+void UINavigator::ExecGame(string game) 
+{
+	//string for path to game exe 
+	std::string fullpath;
+
+	//temp string for checking if a given file is exe
+	std::string exeStr;
+
+	//string var to attach to path in order to execute exe
+	std::string exeFile;
+
+	//Have to edit hardcoded path for aracade cabinet games folder 
+	std::string path = "//FRANK/U/jkiger/Documents/Games";
+
+	//string building start of path to loop over in order to find .exe
+	fullpath = path + "/" + game;
+
+	//Loop over all files in the given file path above
+	for (const auto & entry : directory_iterator(fullpath)) 
+	{
+
+		//grab all file extensions that exist in the current directory
+		exeStr= entry.path().filename().extension().string();
+
+		//check if given file extension is .exe
+		if (strcmp(exeStr.c_str(),".exe") == 0) 
+		{
+
+			  //set string to found .exe file
+			  exeFile = entry.path().filename().string();
+		}
+	}
+
+	//string build the rest of the path 
+	fullpath += "/" + exeFile;
+
+	//execute the executable for the game
+	system(fullpath.c_str());
+}
---
+++ UINavigator.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <filesystem>
+#include <vector>
+#include <stdio.h>
+#include <Windows.h>
+
+using namespace std;
+using namespace std::experimental::filesystem;
+
+class UINavigator
+{
+public:
+
+	// Within the game directory, return the name of each game within a vector. 
+	// A vector is like a doubly linked array structure within c++.
+	vector<string> GameList();
+
+	// This function will take the name of the game, go into its folder, and execture the
+	// game for the user to play.
+	void ExecGame(string game);
+
+};
+
